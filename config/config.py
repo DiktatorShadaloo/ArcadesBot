ebb17ca675f6e9f79d3b530870070def2a2026cb
DiktatorShadaloo <diktatorshadaloo@gmail.com>
2022-04-01 17:28:46 -0300

    Agregada fucnion de mandar mensajes automaticos.

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 import configparser
+import string
 import requests
 from pathlib import Path
 import json
@@ -19,6 +20,17 @@ def getChannelID( access_token, client_id):
     data = response.json()['data'][0]
     return int(data['id'])
 
+def getReward_ID(channel_id, channel_token, client_id, reward_name):
+    headers = {
+    'Client-ID': client_id,
+    'Authorization': 'Bearer %s' % (channel_token)
+    }
+    response = requests.get('https://api.twitch.tv/helix/channel_points/custom_rewards?broadcaster_id=%s'% (channel_id), headers=headers)
+    data = response.json()['data']
+    for x in data:
+        if reward_name.lower() == x['title'].lower():
+            return x['id']
+
 BOT_NICK = configParser.get('BOT', 'bot_nick')
 BOT_TOKEN = configParser.get('BOT', 'bot_token')
 BOT_PREFIX = configParser.get('BOT', 'bot_prefix')
@@ -28,10 +40,13 @@ CHANNEL_TOKEN = configParser.get('CHANNEL', 'channel_token')
 CLIENT_ID = configParser.get('CHANNEL', 'client_id')
 CHANNEL_ID = getChannelID(CHANNEL_TOKEN,CLIENT_ID)
 REWARD_INSERT_COINS = configParser.get('CHANNEL', 'reward_insert_coins')
+INSERT_COINS_ID = getReward_ID(CHANNEL_ID, CHANNEL_TOKEN,CLIENT_ID, REWARD_INSERT_COINS)
 REWARD_BUY_COIN = configParser.get('CHANNEL', 'reward_buy_coin')
+BUY_COIN_ID = getReward_ID(CHANNEL_ID, CHANNEL_TOKEN,CLIENT_ID, REWARD_BUY_COIN)
 MODS = list(configParser['MODS'].keys())
 PRECIO_BITS = int(configParser.get('CHANNEL', 'precio_bits'))
 FICHAS_X_SUB = int(configParser.get('CHANNEL', 'fichas_x_sub'))
 PRECIO_FICHA = float(configParser.get('CHANNEL', 'precio_ficha'))
 FICHAS_MAXIMAS = int(configParser.get('CHANNEL', 'fichas_maximas'))
 BANNED_MESSAGES = list(configParser['BANNED_MESSAGES'].keys())
+AUTOMATIC_MESSAGES = list(configParser['AUTOMATIC_MESSAGES'].keys())
--- src/irc.py
+++ src/irc.py
@@ -1,3 +1,4 @@
+from random import randint
 import socket
 import re
 from datetime import datetime
@@ -13,26 +14,30 @@ PASS = "oauth:" + BOT_TOKEN
 HOST = "irc.chat.twitch.tv"
 PORT = 6667
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
-sock.send("PASS {}\r\n".format(PASS).encode("utf-8"))
-sock.send("NICK {}\r\n".format(NICK).encode("utf-8"))
-sock.send("JOIN {}\r\n".format(CHANNEL[0].lower()).encode("utf-8"))
+def connect():
+     sock = socket.socket()
+     sock.connect((HOST, PORT))
+     sock.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+     sock.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+     sock.send("JOIN {}\r\n".format(CHANNEL[0].lower()).encode("utf-8"))
+     return sock
+
 
 def reconnect():
      it =0
      connected = False
-     global sock
      sock = socket.socket()
      printear( "Conexion del cliente perdida, intentando reconectar..." ) 
      while connected == False and it <= 15:   
           try:  
-               sock2 = socket.socket()
-               sock2.connect((HOST, PORT))
-               sock2.send("PASS {}\r\n".format(PASS).encode("utf-8"))
-               sock2.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+               sock = socket.socket()
+               sock.connect((HOST, PORT))
+               sock.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+               sock.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+               sock.send("JOIN {}\r\n".format(CHANNEL[0].lower()).encode("utf-8"))
                connected = True  
                printear( "reconexion exitosa" )  
+               return sock
           except socket.error:  
                sleep(1)
                it = it + 1
@@ -41,15 +46,11 @@ def reconnect():
 
 # Envia un mensaje al chat
 def chat(MENSAJE):
-     sock2 = socket.socket()
-     sock2.connect((HOST, PORT))
-     sock2.send("PASS {}\r\n".format(PASS).encode("utf-8"))
-     sock2.send("NICK {}\r\n".format(NICK).encode("utf-8"))
-     sock2.send("JOIN {}\r\n".format(CHANNEL[0].lower()).encode("utf-8"))
+     sock2 = connect()
      try:
           sock2.send(("PRIVMSG %s :" % CHANNEL[0].lower() + MENSAJE +"\r\n").encode("utf-8"))
      except socket.error:
-          reconnect()
+          sock2 = reconnect()
           sock2.send(("PRIVMSG %s :" % CHANNEL[0].lower() + MENSAJE +"\r\n").encode("utf-8"))
 
 # Banea a un usuario
@@ -62,6 +63,17 @@ def ban(USER):
           reconnect()
           ban(USER)
      '''
+def mensajeProgramado(tiempo):
+     it= randint(0, len(AUTOMATIC_MESSAGES)-1)
+     starting_time = datetime.now()
+     while True:
+          difference = datetime.now() - starting_time
+          if difference.total_seconds() > tiempo:
+               MENSAJE = AUTOMATIC_MESSAGES[it]
+               chat(MENSAJE)
+               it= (it+1) % len(AUTOMATIC_MESSAGES)
+               starting_time = datetime.now()
+
 # Funcion concurrente para hacer homenaje al caido Fichinbot U_U
 def RIP ():
      RIP_time = datetime.now()
@@ -84,6 +96,7 @@ def repo():
 
 # Funcion concurrente para enviar PING al servidor y asi evitar desconexiones o intentar reconectar mas frecuentemente.
 def PING():
+     global sock
      PING_time = datetime.now()
      while True:
           difference = datetime.now() - PING_time
@@ -92,30 +105,39 @@ def PING():
                     sock.send("PING :tmi.twitch.tv\r\n".encode("utf-8"))
                     PING_time = datetime.now()
                except socket.error:
-                    reconnect()
+                   sock = reconnect()
+
+sock = connect()
 
 def run_IRC():
+     global sock
      RIPthread = Thread(target = RIP, args = ())
-     #REPOthread = Thread(target = repo, args = ())
-     PINGthread = Thread(target = PING, args = ())
      RIPthread.start()
+     #REPOthread = Thread(target = repo, args = ())
      #REPOthread.start()
+     PINGthread = Thread(target = PING, args = ())
      PINGthread.start()
+     if len (AUTOMATIC_MESSAGES) >0:
+          #Cada cuantos segundos se enviara un mensaje automatico.
+          tiempo = 3600
+          MENSAJESthread = Thread(target = mensajeProgramado, args = (tiempo,))
+          MENSAJESthread.start()
      while True:
-          response = sock.recv(1024).decode("utf-8")
-          if response == "PING :tmi.twitch.tv\r\n":
-               sock.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-          else:
-               username = re.search(r"\w+", response).group(0)
-              
-               for x in BANNED_MESSAGES:
-                    if x.lower() in response.lower():
-                         ban(username)
-                         msg = "%s, agarra todo eso, hace un rollito y metetelo bien por el ****" % username
-                         chat(msg)
-                         break
-
-
+          try:
+               response = sock.recv(1024).decode("utf-8")
+               if response == "PING :tmi.twitch.tv\r\n":
+                    sock.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+               else:
+                    username = re.search(r"\w+", response).group(0)
+               
+                    for x in BANNED_MESSAGES:
+                         if x.lower() in response.lower():
+                              ban(username)
+                              msg = "%s, agarra todo eso, hace un rollito y metetelo bien por el ****" % username
+                              chat(msg)
+                              break
+          except socket.error:
+               sock = reconnect()
 
 
 def main_IRC():
